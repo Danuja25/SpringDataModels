649631d223119d10c82e414498068541ab264c20
danuja25 <danujajayawar@gmail.com>
2021-12-15 22:12:35 +0000

    SDM:SDM=0001:Included spring data rest

--- user-library/src/main/java/org/danuja25/springdata/user/repository/UserGroupRepository.java
+++ user-library/src/main/java/org/danuja25/springdata/user/repository/UserGroupRepository.java
@@ -2,7 +2,9 @@ package org.danuja25.springdata.user.repository;
 
 import org.danuja25.springdata.user.entity.UserGroupEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(collectionResourceRel = "userGroups", path = "userGroups")
 public interface UserGroupRepository extends JpaRepository<UserGroupEntity, Long> {
 
     UserGroupEntity findByCode(String groupCode);
--- user-library/src/main/java/org/danuja25/springdata/user/repository/UserRepository.java
+++ user-library/src/main/java/org/danuja25/springdata/user/repository/UserRepository.java
@@ -4,18 +4,14 @@ import org.danuja25.springdata.user.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import static org.danuja25.springdata.user.UserConstant.SG_USER_TABLE_NAME;
 
+@RepositoryRestResource(collectionResourceRel = "users", path = "users")
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
-
-//    @Query("SELECT t FROM " + org.danuja25.springdata.UserConstant.USER_TABLE_NAME + " t where t.username = :username")
-//    @Query(value = "SELECT * FROM " + org.danuja25.springdata.UserConstant.USER_TABLE_NAME + " t where t.username = ?0", nativeQuery=true)
-//    String findUserByUsername(@Param("username") String username);
 
     UserEntity findByUsername(String username);
 
-    @Query(value = "SELECT t FROM " + SG_USER_TABLE_NAME + " t where t.username = :username and t.password = :password")
-    public UserEntity findByCredentials(@Param("username") String username, @Param("password") String password);
 
 }
